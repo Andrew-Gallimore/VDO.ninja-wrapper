eeb8229921f345cd102d1df967a95603b46169b2
Andrew_Gallimore <gallimoreandrew04@gmail.com>
2023-01-04 20:13:08 -0800

    Mid-new stuff

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require('express')
 const https = require('https')
 const path = require('path')
 const fs = require('fs')
+const devcert = require('devcert')
 
 const app = express();
 app.get('/', function(req, res) {
@@ -9,13 +10,21 @@ app.get('/', function(req, res) {
 });
 app.use('/', express.static(__dirname + '/'));
 
-const sslServer = https.createServer(
-    {
-      key: fs.readFileSync(path.join(__dirname, 'SSLcert', 'server.key')),
-      cert: fs.readFileSync(path.join(__dirname, 'SSLcert', 'server.crt')),
-    },
-    app
-)
+// const sslServer = https.createServer(
+//     {
+//       key: fs.readFileSync(path.join(__dirname, 'SSLcert', 'server.key')),
+//       cert: fs.readFileSync(path.join(__dirname, 'SSLcert', 'server.crt')),
+//     },
+//     app
+// )
 
-var port = process.env.PORT || 443;
-sslServer.listen(port, () => console.log("Server Started.", "\x1b[1m", "\x1b[36m", "(Port: " + port + ")", "\x1b[0m"))
+async function createServer() {
+  let ssl = await devcert.certificateFor('my-app.test');
+  console.log(ssl)
+  const sslServer = https.createServer(ssl, app);
+
+  var port = process.env.PORT || 443;
+  sslServer.listen(port, () => console.log("Server Started.", "\x1b[1m", "\x1b[36m", "(Port: " + port + ")", "\x1b[0m"))
+}
+
+createServer();
